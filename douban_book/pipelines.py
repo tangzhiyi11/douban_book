adaf3b517d32105f25a71ab0f4f2c6312f9611ae
tangzhiyi <854742740@qq.com>
2016-10-12 14:31:19 +0800

    add codes

--- douban_book/pipelines.py
+++ douban_book/pipelines.py
@@ -4,8 +4,13 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from items import DoubanBookItem
 
 
 class DoubanBookPipeline(object):
     def process_item(self, item, spider):
+        print item['title']
+        print item['rate']
+        print item['author']
+        print '--------'
         return item
